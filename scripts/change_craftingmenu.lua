6d0232636997295811966897cda8669f5c94df49
程小黑 <chengxiaohei2@gmail.com>
2024-08-03 20:42:32 +0800

    Sync with Official New Update Content

--- scripts/change_craftingmenu.lua
+++ scripts/change_craftingmenu.lua
@@ -373,7 +373,7 @@ AddClassPostConstruct("widgets/redux/craftingmenu_details", function(self)
             elseif not builder:CanLearn(recipe.name) then
                 -- If our recipe's builder tag is a skilltree tag, check if we're the skill tree owner,
                 -- and choose a string based on that.
-                str = (recipe.builder_tag ~= nil and self.owner.prefab == TECH_SKILLTREE_BUILDER_TAG_OWNERS[recipe.builder_tag] and STRINGS.UI.CRAFTING.NEEDSCHARACTERSKILL)
+                str = (recipe.builder_skill ~= nil and self.owner.components.skilltreeupdater:IsValidSkill(recipe.builder_skill)) and STRINGS.UI.CRAFTING.NEEDSCHARACTERSKILL
                     or STRINGS.UI.CRAFTING.NEEDSCHARACTER
             else
                 local prototyper_tree = self:_GetHintTextForRecipe(self.owner, recipe)
